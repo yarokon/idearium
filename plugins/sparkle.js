44413247c84fb1062ce93a42fe5ece2b659358c9
Yaroslav Shuhailo <yaroslav@redocly.com>
2024-08-22 08:56:51 +0000

    add sparkle plugin

---
+++ plugins/sparkle.js
@@ -0,0 +1,12 @@
+const OperationSparkle = require('./decorators/operation-sparkle.js');
+
+module.exports = function sparklePlugin() {
+  return {
+    id: "sparkle",
+    decorators: {
+      oas3: {
+        "operation-sparkle": OperationSparkle,
+      },
+    },
+  };
+}
