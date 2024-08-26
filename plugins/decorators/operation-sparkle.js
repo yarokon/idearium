b46fad59af2fae0a31b6362d9d8ce32f5b93cf4a
Yaroslav Shuhailo <yaroslav@redocly.com>
2024-08-26 06:43:20 +0000

    Update realm

--- plugins/decorators/operation-sparkle.js
+++ plugins/decorators/operation-sparkle.js
@@ -7,7 +7,7 @@ function OperationSparkle() {
     Operation: {
       leave(target) {
         if(target.description) {
-          target.description = "✨ " + String(target.description);
+          target.description = "✨123 " + String(target.description);
         }
       }
     },
--- plugins/sparkle.js
+++ plugins/sparkle.js
@@ -1,21 +1,21 @@
 const OperationSparkle = require('./decorators/operation-sparkle.js');
 
-// module.exports = function sparklePlugin() {
-//   return {
-//     id: "sparkle",
-//     decorators: {
-//       oas3: {
-//         "operation-sparkle": OperationSparkle,
-//       },
-//     },
-//   };
-// }
-
-module.exports = {
-  id: "sparkle",
-  decorators: {
-    oas3: {
-      "operation-sparkle": OperationSparkle,
+module.exports = function sparklePlugin() {
+  return {
+    id: "sparkle",
+    decorators: {
+      oas3: {
+        "operation-sparkle": OperationSparkle,
+      },
     },
-  },
-};
+  };
+}
+
+// module.exports = {
+//   id: "sparkle",
+//   decorators: {
+//     oas3: {
+//       "operation-sparkle": OperationSparkle,
+//     },
+//   },
+// };
